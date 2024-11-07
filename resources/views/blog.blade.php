aa1b7a056b18fb5fabe8f14bc54cb7b994d89662
ansfjk <botf7060@gmial.com>
2024-11-07 19:57:59 +0700

    7:57

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>blog</title>
+    <title>blog page</title>
 </head>
 <body>
-    
+    <a href="/">home</a>
+    <a href="/blog">blog</a>
+    <a href="/about">about</a>
+    <a href="/contact">contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>
+            judul 1
+        </h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed magni ut, aperiam commodi sunt, 
+        qui labore quam quidem autem quia maiores, officia optio aliquam praesentium. Aspernatur ad quia distinctio nemo.
+    </article>
+    <article>
+        <h3>
+            judul 2
+        </h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed magni ut, aperiam commodi sunt, 
+        qui labore quam quidem autem quia maiores, officia optio aliquam praesentium. Aspernatur ad quia distinctio nemo.
+    </article>
 </body>
 </html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>about</title>
+    <title>Contact</title>
 </head>
 <body>
     <a href="/">home</a>
     <a href="/blog">blog</a>
     <a href="/about">about</a>
     <a href="/contact">contact</a>
+    <ul>
+        <li><a href="">...</a></li>
+        <li><a href="">...</a></li>
+        <li><a href="">...</a></li>
+    </ul>
 </body>
 </html>
