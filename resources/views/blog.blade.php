622a2b8ec9bcb58b9aab19d94f4aa2acc4b6cdec
ansfjk <botf7060@gmial.com>
2024-11-08 17:55:42 +0700

    5:55

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,20 @@
 <x-layout>
   <x-slot:title>{{$title}}</x-slot:title>
   <h3 class="text-xl"></h3>
+  @foreach($posts as $post)
   <article class="py-8 max-w-screen-md border-b border-gray-300">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold">Judul artikel1</h2>
+    <h2 class="mb-1 text-3xl tracking-tight font-bold">{{$post['title']}}</h2>
     <div class="text-base text-gray-500">
-      <a href="">Beliau</a>
+      <a href="">{{$post['author']}}</a>
     </div>
     <div>
      <p class="my-4 font-light">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure, quibusdam libero repellat sunt nulla doloremque autem,
-        recusandae iusto voluptatibus eaque reiciendis delectus perferendis dolor. Ipsam id accusamus quibusdam porro!
+       {{Str::limit($post['body'],52)}}
      </p>
     </div>
+    <a>
+    
+    </a>
   </article>
+  @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,27 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About age']);
 });
 Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']); // Menyisipkan variabel 'title' ke dalam view
+    return view('blog', ['title' => 'Blog Page','posts' => [
+        [
+            'id' => '1',
+            'title' => 'Judul artikel1',
+            'author' => 'Beliau',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure, quibusdam libero repellat sunt nulla doloremque autem,
+             recusandae iusto voluptatibus eaque reiciendis delectus perferendis dolor. Ipsam id accusamus quibusdam porro!'
+        ],
+        [
+            'id' => '2',
+            'title' => 'Judul artikel2',
+            'author' => 'Beliau',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure, quibusdam libero repellat sunt nulla doloremque autem,
+             recusandae iusto voluptatibus eaque reiciendis delectus perferendis dolor. Ipsam id accusamus quibusdam porro!'
+        ]
+    ]]); // Menyisipkan variabel 'title' ke dalam view
 });
+Route::get('/blog/{{id}}', function () {
+    
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact age']) ;
 });
