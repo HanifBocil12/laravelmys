f61b1537982d64fd74be7fde57588a3cba82ebfb
ansfjk <botf7060@gmial.com>
2024-11-08 17:27:07 +0700

    5:26

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,16 @@
 <x-layout>
   <x-slot:title>{{$title}}</x-slot:title>
   <h3 class="text-xl"></h3>
+  <article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold">Judul artikel1</h2>
+    <div class="text-base text-gray-500">
+      <a href="">Beliau</a>
+    </div>
+    <div>
+     <p class="my-4 font-light">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iure, quibusdam libero repellat sunt nulla doloremque autem,
+        recusandae iusto voluptatibus eaque reiciendis delectus perferendis dolor. Ipsam id accusamus quibusdam porro!
+     </p>
+    </div>
+  </article>
 </x-layout>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,5 @@
-<a {{$attributes}}
+@props(['active' => false])
+<a
 class="rounded-md 
               {{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white' }} 
-               px-3 py-2 text-sm font-medium" aria-current="{{$active ? 'page' : false}}">{{slot}}</a>
+               px-3 py-2 text-sm font-medium" aria-current="{{$active ? 'page' : false}}" {{$attributes}}>{{slot}}</a>
